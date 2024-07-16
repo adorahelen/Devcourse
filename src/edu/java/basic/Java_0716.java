479043dd3105538238913445e0e9b7c4672f533e
adorahelen <adorahelenmin@gmail.com>
2024-07-16 16:22:12 +0900

    DO or Die

---
+++ src/edu/java/basic/Java_0716.java
@@ -0,0 +1,62 @@
+package edu.java.basic;
+
+public class Java_0716 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+
+        boolean tf = true;
+        char c = 'a';
+        tf = 1<0;
+        tf = true;
+        tf =false;
+        c = 66;
+        tf = true;
+        System.out.println(tf);
+        System.out.println(c);
+
+        int s;
+        int i = 20;
+       s = Hello.sum(i, 10);
+        System.out.println((s));
+
+
+        int eng = 80;
+        int com = 75;
+        int korea = 90;
+
+        double sum = 0;
+        double avg = 0;
+        String name = "kim kang min";
+        sum = eng + com + korea;
+        avg = sum / 3;
+
+        System.out.println("name is" + name);
+        System.out.println("eng is" + eng);
+        System.out.println("com is" + com);
+        System.out.println("korea is" + korea);
+        System.out.println("sum is " + sum);
+        System.out.println("avg is " + avg);
+
+
+        var guess = -1;
+        var guess2 = "adasdadasdas";
+
+        // 상수 선언의 경우 final 을 앞에 선언한다.
+        final double  PI = 3.14;
+
+
+    }
+
+    // Hello 클래스는 Java_0716 클래스의 멤버 클래스
+    public static class Hello {
+        public static int sum(int n, int m) {
+            return  n+m;
+        }
+    }
+    public static class Score {
+        public static int sum(int n, int m, int s) {
+            return n+m+s;
+        }
+    }
+}
---
+++ src/edu/java/basic/Java_0716_TypeCasting.java
@@ -0,0 +1,36 @@
+package edu.java.basic;
+
+public class Java_0716_TypeCasting {
+    public static void main(String[] args) {
+        // 스트링으로 읽어 들인다, 알규먼트는 하나씩
+
+        byte one = 1;
+        byte two = 2;
+        byte three = (byte)(one + two);
+        int three2 = one + two;
+
+        int oneMil = 1000000;
+        int twoMil = 2000000;
+
+        long oneMilLong = (long)1000000 * 2000000;
+
+        System.out.println(oneMilLong);
+
+        System.out.println(one);
+        System.out.println(two);
+        System.out.println(three);
+    }
+}
+//  변수에 값을 대입할 때
+// 피연산자의 타입이 같아야 연산 가능
+// 연산 전에 피연사자들의 타입을 일치시킴
+// 자료형끼리의 연산에 주의해야한다! 연산이 안되거나 값 손실이 일어날 수 도 있음
+
+// 자동형변환 업 캐스팅. jmv 내부에서 수행
+// byte, short, char  -> int -> long , float , double
+// 데이터 타입이 다른 경우. 표현 범위가 넓은 쪽에 맞춰 변환된 후 연산 수행
+// int + float >>> float + float >>> 결과적으로는 float
+// int + long >>> long + long >>> long
+
+// 반대로 다운 캐스팅의 경우
+// 범위가 작은 자료형으로 바꿀때, 값 손실 발생이 가능하다. (소수점)
