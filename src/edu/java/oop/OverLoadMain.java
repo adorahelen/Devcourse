496ccabc7eecddc1777c5ea613169042c12d5edd
adorahelen <adorahelenmin@gmail.com>
2024-07-19 12:28:01 +0900

    Hungrry

--- src/edu/java/oop/OverLoadMain.java
+++ src/edu/java/oop/OverLoadMain.java
@@ -19,6 +19,21 @@ public class OverLoadMain {
         System.out.println(Sam.area(5, 5));
         System.out.println(Sam);
         System.out.println(Sam.toString());
-        System.out.println(Sam);
+
+        System.out.println("---");
+        Sam.sumAll();
+        // static 으로 선언된 메서드는 객체를 이용하지 않고 사용이 가능하다
+        OverLoading.sumAll();// 객체를 이용하지 않고 클래스 이름으로 접근하여 사용가능
+        System.out.println("1 + 2 = " + OverLoading.sumAll(1 , 2));
+        System.out.println("x + y + z = " + OverLoading.sumAll(1 , 2 , 3));
+        int[] nums = {1,2,3,4,5,6,7,8,9,10};
+        double[] nums2 = {1,2,3,4,5,6,7,8,9,10};
+        System.out.println(OverLoading.sumAll(nums));
+        System.out.println(OverLoading.sumAll(1.0, 2.0, 3.0));
+        System.out.println(OverLoading.sumAll(nums2));
+
+        System.out.println(won.PI);
+        System.out.println(OverLoading.PI);
+
     }
 }
--- src/edu/java/oop/OverLoading.java
+++ src/edu/java/oop/OverLoading.java
@@ -4,12 +4,14 @@ public class OverLoading {
 
     public static final double PI = 3.14;
     // 접근가능 - 공유가능 - 상수임 - 더블 자료형 - 이름은 파이
+    // 스태틱으로 필드는 선언시 반드시 초기화 필요
     private String type;
 
 
     public OverLoading(String type) {
         // 타입의 값을 매개변수로 받아서 초기화
         this.type = type;
+        // 공유 필드 == 스태틱은 => 모든 객체가 공유하기 때문에 각 생성자에서 개별적으로 초기화할 수 없다.
     }
 
 
@@ -41,5 +43,39 @@ public class OverLoading {
     //  현재 객체를 종류나 문자열의 형태로 반환하는 메서드
     public String toString() {
         return "종류 :  " +  type  +  ", " + "dnjswndbf " + PI;
+    }
+
+
+
+    // var args - 공유 메서드 sumAll () 오버로딩
+    public static void sumAll () {
+        System.out.println(" === All of sum ===");
+
+    }
+
+    // 정수 2개를 매개변수로 받아서 더한 후 에 반환하는 공유 메서드
+    public static int sumAll(int x, int y) {
+        return x + y;
+    }
+
+    public static int sumAll(int xx, int yy, int zz) {
+        return xx + yy + zz;
+    }
+
+    // 정수 배열을 매개 변수로 받아서 더한 후 반환하는 공유 메서드
+    public static int sumAll(int x[]) {
+        int sum = 0;
+        for (int i = 0; i < x.length; i++) {
+            sum += x[i];
+        }
+        return sum;
+    }
+    // 매개변수 쪽에 똑같으면 문제가 생기니까
+    public static double sumAll(double ... nums) {
+        double sum = 0;
+        for (double i : nums) {
+            sum += i;
+        }
+        return sum;
     }
 }
