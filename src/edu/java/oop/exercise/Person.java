4d2b7c73c233aec5dacb8b21021bb8d0d4dd6bf4
adorahelen <adorahelenmin@gmail.com>
2024-07-19 17:54:10 +0900

    args?

--- src/edu/java/oop/exercise/Person.java
+++ src/edu/java/oop/exercise/Person.java
@@ -1,4 +1,42 @@
 package edu.java.oop.exercise;
 
+// 필드들은 객체 내부에서만 접근 가능 => 프라이빗 써라
+// 필드들의 값을 매개변수로 받아서 초기화하는 생성자 = > 게터
+// 객체 외부에서 필드에 접근 가능하도록 메서드들을 정의 = > 세터
+// 객체의 필드값들을 문자열로 반환하는 메서드 정ㅇ => 투스트링
+
 public class Person {
+    public Person() {}
+
+    private String name;
+    private String tell;
+
+    Person(String name, String number) {
+        this.name = name;
+        this.tell = tell;
+    }
+    // 겟터 셋터 써야 했음
+
+// command + N
+    public String getName() {
+        return name;
+    }
+    public String getTel() {
+        return tell;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setTel(String tel) {
+        this.tell = tel;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", tel='" + tell + '\'' +
+                '}';
+    }
 }
---
+++ src/edu/java/oop/exercise/PersonMain.java
@@ -0,0 +1,50 @@
+package edu.java.oop.exercise;
+
+public class PersonMain {
+    // 펄슨 객체를 매개변수로 받아서 이름과 전화번호를 화면에 표시하는
+    // 공유 메서드 PersonInfo () 작성
+
+    public void Personinfo(Person person) {
+        System.out.println("----연락처 정보---");
+        System.out.println("이름 : " + person.getName());
+        System.out.println("전화번호 : " + person.getTel());
+        System.out.println();
+//        System.out.println(person);
+//        System.out.println(person2);
+    }
+    // 반환값 : x
+    // 출력형태 : =====연락처정보======
+    //            이름 :
+    //            전화번호 :
+
+    public static void main(String[] args) {
+//        Person person = new Person("나루토", "010-1234-5678");
+//        Person person2 = new Person("사스케", "010-1234-5678");
+//
+        Person person1 = new Person(args[0], args[1]);
+
+//        Person person2 = new Person(args[1], args[2]);
+
+        PersonMain pm = new PersonMain();
+        pm.Personinfo(person1);
+
+        Person person2 = new Person();
+        person2.setName("Han");
+        person2.setTel("1888888888");
+        pm.Personinfo(person2);
+
+
+        // 명령해 매개변수 이용해서
+        // 프로그램 실행시 이름과 전화번호를 전달받고
+        // 전달 받은 값으로 펄슨 객체를 생성한 후
+        // 정보룰 화면에 출력하는 메서드를 호출
+
+
+        // Person 객체 Person 2 생성
+        //
+
+
+    }
+}
+
+
